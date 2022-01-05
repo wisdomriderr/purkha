9a75905fba1334f951630669eab8e04a5c15e27f
wisdomriderr <developerwisdomrider@gmail.com>
2022-01-05 07:56:37 +0545

    3 files modified, 2 added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
-import * from '../style.css'
+import json from '../data.json';
 
-export default function Home() {
-  return <div className={styles.container}>ABC</div>;
+export default function Home({ people }) {
+  return <div>{JSON.stringify(people)}</div>;
+}
+export async function getServerSideProps(context) {
+  const generations = {};
+  json.data.people.forEach((e) => {
+    generations[e.name] = json.data.people.filter(
+      (x) => x.personreference && x.personreference.name === e.name
+    );
+  });
+
+  console.log(generations);
+
+  return {
+    props: {
+      people: generations,
+    },
+  };
 }
